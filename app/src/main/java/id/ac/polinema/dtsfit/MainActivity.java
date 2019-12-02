739acf0d6155270799e2f132795098b1da9fbcd7
fawwazrd <frayd1999@gmail.com>
2019-12-02 16:47:23 +0700

    REST-IMPLENENTASI SAVE CALORYFRAGMENT

--- app/src/main/java/id/ac/polinema/dtsfit/MainActivity.java
+++ app/src/main/java/id/ac/polinema/dtsfit/MainActivity.java
@@ -11,11 +11,21 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+import java.util.Locale;
+
 import id.ac.polinema.dtsfit.adapters.CaloriesAdapter;
 import id.ac.polinema.dtsfit.fragments.CaloryFragment;
 import id.ac.polinema.dtsfit.fragments.ProfileFragment;
 import id.ac.polinema.dtsfit.fragments.SaveCaloryFragment;
+import id.ac.polinema.dtsfit.generator.ServiceGenerator;
 import id.ac.polinema.dtsfit.models.Calory;
+import id.ac.polinema.dtsfit.services.CaloryService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements
 		CaloryFragment.OnFragmentInteractionListener,
@@ -24,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements
 	private Profile profile;
 
 	// TODO: Definisikan CaloryService caloryService
+	private CaloryService caloryService;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements
 		Profile profile = Application.provideProfile();
 
 		// TODO: Instansiasi nilai caloryService dengan menggunakan ServiceGenerator
+		caloryService = ServiceGenerator.createService(CaloryService.class);
 
         Fragment startFragment = (profile.getBmr() != 0)
 				? CaloryFragment.newInstance()
@@ -87,20 +100,56 @@ public class MainActivity extends AppCompatActivity implements
 	@Override
 	public void onCaloryFragmentCreated(final View view, final CaloriesAdapter adapter, final TextView caloryText) {
 		// TODO: Implementasikan load data calory
+		Call<List<Calory>> caloriesCall = caloryService.getCalories();
+		caloriesCall.enqueue(new Callback<List<Calory>>() {
+			@Override
+			public void onResponse(Call<List<Calory>> call, Response<List<Calory>> response) {
+				List<Calory> calories = response.body();
+				adapter.setCalories(calories);
+
+				// Tambahkan logic di baris ini untuk mengkalkulasi total calory
+				int total = 0;
+				for (Calory calory : calories) {
+					total += calory.getCalory();
+				}
+				caloryText.setText(String.format(Locale.ENGLISH,"Your calory %d cal", total));
+			}
+
+			@Override
+			public void onFailure(Call<List<Calory>> call, Throwable t) {
+				Snackbar.make(view, "Oops!", Snackbar.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 	@Override
 	public void onAddCaloryButtonClicked() {
         // TODO: Implementasikan aksi tombol tambah calory pada CaloryFragment
+		changeFragment(SaveCaloryFragment.newInstance(null));
 	}
 
 	@Override
 	public void onCaloryClicked(Calory calory) {
         // TODO: Implementasikan aksi item calory ketika dipilih pada CaloryFragment
+		changeFragment(SaveCaloryFragment.newInstance(calory));
 	}
 
-	@Override
-	public void onSaveMenuClicked(final View view, Calory calory) {
-		// TODO: Implementasikan aksi ketika menu simpan ditekan pada SaveCaloryFragment
+		@Override
+		public void onSaveMenuClicked(final View view, Calory calory) {
+			Call<Calory> caloryCall = (calory.getId() == null)
+					? caloryService.addCalory(calory)
+					: caloryService.editCalory(calory.getId(), calory);
+			caloryCall.enqueue(new Callback<Calory>() {
+				@Override
+				public void onResponse(Call<Calory> call, Response<Calory> response) {
+					Snackbar.make(view, "Save successfull", Snackbar.LENGTH_SHORT).show();
+					changeFragment(CaloryFragment.newInstance());
+				}
+
+				@Override
+				public void onFailure(Call<Calory> call, Throwable t) {
+					Snackbar.make(view, "Error has occured!", Snackbar.LENGTH_SHORT).show();
+				}
+			});
+		}
 	}
-}
---
+++ app/src/main/java/id/ac/polinema/dtsfit/services/AService.java
@@ -0,0 +1,6 @@
+package id.ac.polinema.dtsfit.services;
+
+public class AService {
+    public AService() {
+    }
+}
